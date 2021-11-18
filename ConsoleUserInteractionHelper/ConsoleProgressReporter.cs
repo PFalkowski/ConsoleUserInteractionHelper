60ed12c3aa6b7fb9c12e0e948e2b07e881b169d6
Piotr Falkowski <pfalkowski@sevillemore.com>
2021-11-18 13:36:08 +0100

    custom progressbar message

--- ConsoleUserInteractionHelper/ConsoleProgressReporter.cs
+++ ConsoleUserInteractionHelper/ConsoleProgressReporter.cs
@@ -18,11 +18,23 @@ namespace ConsoleUserInteractionHelper
             _progressBar = new ProgressBar((int)targetValue);
             base.Restart(targetValue);
         }
-
+        
         public override void ReportProgress(double rawProgressValue)
         {
             _progressBar.Refresh((int)rawProgressValue, $"Remaining {RemainingTimeEstimate.ToString("hh\\:mm\\:ss")}");
             base.ReportProgress(rawProgressValue);
+        }
+        
+        public override void ReportProgress(double rawProgressValue, string customProgressMessage)
+        {
+            _progressBar.Refresh((int)rawProgressValue, $"{customProgressMessage} {RemainingTimeEstimate.ToString("hh\\:mm\\:ss")}");
+            base.ReportProgress(rawProgressValue, customProgressMessage);
+        }
+
+        public override void ReportProgress(string customProgressMessage)
+        {
+            base.ReportProgress(customProgressMessage);
+            _progressBar.Refresh((int)base.CurrentRawValue, $"{customProgressMessage} {RemainingTimeEstimate.ToString("hh\\:mm\\:ss")}");
         }
     }
 }
